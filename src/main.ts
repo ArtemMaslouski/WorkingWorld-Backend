5fc77873d1ef1156bba8ddb55f622d0240385fca
Artsiom <masloartem20@gmail.com>
2025-01-22 18:11:54 +0300

    create base swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Working World')
+    .setDescription('This documentation is created for Frontend Developer, that can deal with backend using this document ')
+    .setVersion('1.0')
+    .addTag('Working World')
+    .build();
+
+    const documetnFactory = () => SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api',app,documetnFactory)
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
