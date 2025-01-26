10a938bbf4953a586f45a6c1a80cee9ce2594bc3
Artsiom <masloartem20@gmail.com>
2025-01-26 17:56:34 +0300

    add recreating access and refresh token when they expires

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -25,6 +25,11 @@ export class AuthController {
     return this.authService.login(userDTO);
   }
 
+  @Post('refresh')
+  async refreshTokens(@Body('refreshToken') token: string){
+    return this.authService.refreshTokens(token);
+  }
+
   @Get('test')
   @UseGuards(AuthGuard)
   @UseGuards(RolesGuard)
