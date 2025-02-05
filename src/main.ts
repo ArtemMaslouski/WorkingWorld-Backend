2591698268ecb974120b8ba4d966a1ed425a59bd
Artsiom <masloartem20@gmail.com>
2025-02-05 19:29:16 +0300

    add global cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{cors: true});
 
   const config = new DocumentBuilder()
     .setTitle('Working World')
