e7bbe861a08867390a8d55351a9301ff513193fe
Artsiom <masloartem20@gmail.com>
2025-02-06 23:20:01 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser'
 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{ cors: true});
   const config = new DocumentBuilder()
     .setTitle('Working World')
     .setDescription('This documentation is created for Frontend Developer, that can deal with backend using this document ')
