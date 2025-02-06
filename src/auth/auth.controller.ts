931787e3f141c2d47af4117894c08ded29ba713a
Artsiom <masloartem20@gmail.com>
2025-02-06 19:10:35 +0300

    add some features to swagger

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { AuthService } from './auth.service';
 import { UserDTO } from 'src/DTO/UserDTO';
 import { AuthGuard } from './guards/auth.guard';
 import { RolesGuard } from './guards/roles.guard';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 
 @Controller('auth')
@@ -12,11 +12,19 @@ import { ApiTags } from '@nestjs/swagger';
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Создание пользователя',
+    description: 'Функция создает пользователя используя имя пользователя, электронную почту и пароль'
+  })
   @Post('create-user')
   async createUser(@Body() userDTO: UserDTO) {
     return this.authService.registerUser(userDTO);
   }
 
+  @ApiOperation({
+    summary: 'Зайти в аккаунт',
+    description: 'Функция позволяет пользователю зайти в аккаунт используя имя пользователя и пароль'
+  })
   @Post('login')
   async login(@Body() userDTO: UserDTO,@Res() res: Response) {
     return this.authService.login(userDTO,res);
@@ -35,6 +43,10 @@ export class AuthController {
     return this.authService.getUsers()
   }
 
+  @ApiOperation({
+    summary: 'Удаление аккаунта',
+    description: 'Функция позволяет удалить аккаунт по логину(возможно позже будем удалять по токену)'
+  })
   @Delete('delete-users')
   async deleteUsers(@Body('Login') Login: string) {
     return this.authService.deleteUser(Login)
@@ -49,16 +61,27 @@ export class AuthController {
     console.log(request.cookies)
   }
 
+  @ApiOperation({
+    summary: 'Отправка кода подтверждения на почту',
+    description: 'Функция позволяет отправить код подтверждения на почту,если пользователь забыл пароль'
+  })
   @Post('send') 
   async sendMail(@Body('Email') email: string) {
     return await this.authService.sendVerificationCodeToEmail(email)
   }
 
+  @ApiOperation({
+    summary: 'Проверка кода,высланного на почту'
+  })
   @Post('forgotPassword')
   async forgotPassword(@Body('Code') code: string){
     return await this.authService.verificateUserWithCodeFromEmail(code)
   }
 
+  @ApiOperation({
+    summary: 'Восстановление пароля',
+    description: 'Функция позволяет создать новый пароль, вместо старого забытого'
+  })
   @Post('resetPassword')
   async resetPassword(@Body('Email') email:string, @Body('Password') password: string ){
     return this.authService.resetPassword(email,password)
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser'
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Working World')
+    .setDescription('This documentation is created for Frontend Developer, that can deal with backend using this document ')
+    .setVersion('1.0')
+    .addTag('Working World')
+    .build();
+
+    const documetnFactory = () => SwaggerModule.createDocument(app,config);
+    SwaggerModule.setup('api',app,documetnFactory)
   app.use(cookieParser())
   await app.listen(process.env.PORT ?? 3000);
 }
