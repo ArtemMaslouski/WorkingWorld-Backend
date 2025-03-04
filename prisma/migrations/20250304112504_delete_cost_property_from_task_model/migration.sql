64e86213979bd6dc7a42ec938f92d91c5306079b
Artsiom <masloartem20@gmail.com>
2025-03-04 14:28:19 +0300

    delete cost property and add getting task endpoint

---
+++ prisma/migrations/20250304112504_delete_cost_property_from_task_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `Cost` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Task" DROP COLUMN "Cost";
