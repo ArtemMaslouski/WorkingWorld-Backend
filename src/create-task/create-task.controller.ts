e66163dded53951f77a1ae3dc07bb05fc7533c45
Artsiom <masloartem20@gmail.com>
2025-03-07 00:30:27 +0300

    add some features to swagger

--- src/create-task/create-task.controller.ts
+++ src/create-task/create-task.controller.ts
@@ -1,16 +1,33 @@
 import { Controller, Body, Post, Get } from '@nestjs/common';
 import { createTaskDTO } from './DTO/create-task-DTO';
 import { CreateTaskService } from './create-task.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { SwaggerResponses } from 'src/auth/configs/swagger-responses.config';
 
 @Controller('create-task')
 export class CreateTaskController {
   constructor(private createTaskService: CreateTaskService) {}
 
+  @ApiOperation({
+    summary: 'Функция для создания задания',
+    description:
+      'Функция позволяет создать задание на основе Категории,подкатегории,Адреса,Описания и временных промежутков',
+  })
+  @ApiResponse(SwaggerResponses.created)
+  @ApiResponse(SwaggerResponses.badRequest)
+  @ApiResponse(SwaggerResponses.serverError)
   @Post('create')
   async createTask(@Body() taskDTO: createTaskDTO) {
     return this.createTaskService.createTask(taskDTO);
   }
 
+  @ApiOperation({
+    summary: 'Функция для получения списка заданий',
+    description: 'Функция позволяет получить список заданий',
+  })
+  @ApiResponse(SwaggerResponses.created)
+  @ApiResponse(SwaggerResponses.badRequest)
+  @ApiResponse(SwaggerResponses.serverError)
   @Get('get')
   async getTasks() {
     return this.createTaskService.getTasks();
