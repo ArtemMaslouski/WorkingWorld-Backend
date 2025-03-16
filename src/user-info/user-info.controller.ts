9bb5ce97d6ceb23984f9de95498ddfeaa6fd68b6
Artsiom <masloartem20@gmail.com>
2025-03-16 19:06:37 +0300

    add telephoneNumber, additional info and ability to change password to userInfo table

--- src/user-info/user-info.controller.ts
+++ src/user-info/user-info.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Body, Get, Request, Post } from '@nestjs/common';
 import { ChangePasswordDTO } from './DTO/change-password-DTO';
 import { UserInfoService } from './user-info.service';
 import * as cookieParser from 'cookie-parser';
+import { AddPhoneNumberDTO } from './DTO/add-phone-number';
+import { AddUserInfoDTO } from './DTO/add-user-info';
 
 @Controller('user-info')
 export class UserInfoController {
@@ -19,5 +21,21 @@ export class UserInfoController {
   @Get('get-info')
   async getUserInfo() {
     return this.userInfoService.getUserInfo();
+  }
+
+  @Post('add-phone-number')
+  async addPhoneNumber(
+    @Request() req,
+    @Body() addPhoneNumberDTO: AddPhoneNumberDTO,
+  ) {
+    const token = req.cookies['access_token'];
+
+    return this.userInfoService.addMobilePhone(token, addPhoneNumberDTO);
+  }
+
+  @Post('add-user-info')
+  async addUserInfo(@Request() req, @Body() addUserInfoDTO: AddUserInfoDTO) {
+    const token = req.cookies['access_token'];
+    return this.userInfoService.addUserInfo(token, addUserInfoDTO);
   }
 }
