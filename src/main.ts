f1181e0b7cd89b44720697f6ed66b8bb5ae7b177
Artsiom <masloartem20@gmail.com>
2025-04-06 11:02:38 +0300

    remove changes

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use(
-    cors({
-      credentials: true,
-      origin: true,
-    }),
-  );
+  app.enableCors();
   const config = new DocumentBuilder()
     .setTitle('Working World')
     .setDescription(
