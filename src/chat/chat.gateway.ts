3c4fd29ba47494256f67cc14599bef0f34f4000d
Artsiom <masloartem20@gmail.com>
2025-07-06 02:45:38 +0300

    feat: add sending message function  and ability to join chat

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,41 +1,75 @@
 import {
   ConnectedSocket,
   MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
 import { ChatService } from './chat.service';
-import { SendMessage } from './DTO/send-message-DTO';
 import { Server, Socket } from 'socket.io';
+import { CreateMessageDTO } from './DTO/create-message-dto';
+import { Logger } from '@nestjs/common';
+import { PrismaService } from '../prisma.service';
 
 @WebSocketGateway({ cors: true })
-export class ChatGateway {
-  @WebSocketServer()
-  server: Server;
-  constructor(private chatService: ChatService) {}
+export class ChatGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() server: Server;
 
-  @SubscribeMessage('sendMessage')
-  async handleMessage(
-    @MessageBody() dto: SendMessage,
-    @ConnectedSocket() client: Socket,
-  ) {
-    const message = await this.chatService.sendMessage(
-      dto.chatId,
-      dto.senderId,
-      dto.content,
-    );
+  constructor(
+    private prismaService: PrismaService,
+    private chatService: ChatService,
+  ) {}
+  private logger = new Logger('ChatGateway');
 
-    this.server.to(`chat-${dto.chatId}`).emit('recieve_message', message);
-
-    return message;
+  afterInit() {
+    this.logger.log('WebSocket Gateway Initialized');
   }
 
+  handleConnection(client: any, ...args: any[]) {
+    this.logger.log(`Client ${client.id} initialized`);
+  }
+
+  handleDisconnect(client: any) {
+    this.logger.log(`Client ${client.id} disconnected`);
+  }
+
+  //Реализация присоединения пользователя к чату
   @SubscribeMessage('joinChat')
-  async joinChat(
-    @MessageBody() chatId: number,
+  handleJoinChat(
+    @MessageBody() data: { chatId: number },
     @ConnectedSocket() client: Socket,
   ) {
-    client.join(`chat-${chatId}`);
+    const room = `chat_${data.chatId}`;
+
+    client.join(room);
+    this.logger.log(`Client ${client.id} joined ${room}`);
+    client.emit('joinedChat', { chatId: data.chatId });
+  }
+
+  //Реализация отправки сообщения другому пользователю
+  @SubscribeMessage('sendMessage')
+  async handleSendMessage(
+    @MessageBody() createMessageDTO: CreateMessageDTO,
+    @ConnectedSocket() client: Socket,
+  ) {
+    try {
+      const message = await this.chatService.createMessage(createMessageDTO);
+
+      const room = `chat_${createMessageDTO.chatId}`;
+
+      this.server.to(room).emit('newMessage', message);
+
+      return { status: 'ok', message };
+    } catch (error) {
+      this.logger.error(`Error sending message: ${error.message}`);
+
+      client.emit('errorMessage', { message: error.message });
+      return { status: 'error', message: error.message };
+    }
   }
 }
