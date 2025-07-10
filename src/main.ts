a39b0fef1443771a7816bf1d032824a91471d50c
Artsiom <masloartem20@gmail.com>
2025-07-11 00:37:55 +0300

    remove swagger to config directory

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
 import { join } from 'path';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { swaggerConfig } from './config/swagger';
 
 async function bootstrap() {
+  //Cors Permisssion
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
     origin: [
@@ -18,27 +20,11 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
-  const config = new DocumentBuilder()
-    .setTitle('Working World')
-    .setDescription(
-      'This documentation is created for Frontend Developer, that can deal with backend using this document ',
-    )
-    .setVersion('1.0')
-    .addTag('Working World')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT',
-        in: 'header',
-        name: 'Authorization',
-      },
-      'JWT',
-    )
-    .build();
 
-  const documetnFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documetnFactory);
+  //Swagger instalation
+  const document = () => SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   app.use(cookieParser());
   app.useStaticAssets(join(__dirname, '..', 'uploads'), {
     prefix: '/uploads',
