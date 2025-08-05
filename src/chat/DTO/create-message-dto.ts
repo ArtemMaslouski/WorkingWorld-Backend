2c0bc4f2426377ca16f5e0f4d9c76c6b5172ae16
Artsiom <masloartem20@gmail.com>
2025-08-06 02:20:40 +0300

    feat: add docs for creating message of chat

--- src/chat/DTO/create-message-dto.ts
+++ src/chat/DTO/create-message-dto.ts
@@ -1,14 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateMessageDTO {
+  @ApiProperty({ description: 'id Чата' })
   @IsInt()
   @IsNotEmpty()
   chatId: number;
 
+  @ApiProperty({ description: 'id отправителя' })
   @IsInt()
   @IsNotEmpty()
   senderId: number;
 
+  @ApiProperty({ description: 'Содержание сообщения' })
   @IsString()
   @IsNotEmpty()
   content: string;
